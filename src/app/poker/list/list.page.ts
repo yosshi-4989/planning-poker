2781d7cd9cc58b352314533ac22eeabb543c224c
yoshida <archer0729@icloud.com>
2019-12-15 18:59:06 +0900

    12/15 検証用ログアウト機能の実装

--- src/app/poker/list/list.page.ts
+++ src/app/poker/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
 
+  signOut() {
+    this.auth.authSignOut();
+  }
 }
