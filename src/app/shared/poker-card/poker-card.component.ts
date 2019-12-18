1635dfb48092528ba9ce9b13b876888eb9afc14c
yoshida <archer0729@icloud.com>
2019-12-19 02:18:54 +0900

    12/19 ポーカーカードのコンポーネント作成

--- src/app/shared/poker-card/poker-card.component.ts
+++ src/app/shared/poker-card/poker-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-poker-card',
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poker-card.component.scss'],
 })
 export class PokerCardComponent implements OnInit {
+  @Input() userName: string;
+  @Input() number: string; // 数字以外も入るため文字列型
+  @Input() userColor: string; // カード色
+  @Input() isOpen: boolean;
+  frontStyle;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // userColorのテーマの値を取得する
+    this.frontStyle = {color: 'var(--ion-color-' + this.userColor + ')'};
+  }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ProfilePage } from './profile/profile.page';
+import { PokerCardComponent } from './poker-card/poker-card.component';
 
 @NgModule({
-  declarations: [ProfilePage],
+  declarations: [ProfilePage, PokerCardComponent],
+  exports: [PokerCardComponent],
   entryComponents: [ProfilePage],
   imports: [
     CommonModule,
