5508a573242dbb7e079b922e4134b16b2b8dd75c
yoshida <archer0729@icloud.com>
2019-12-22 16:05:08 +0900

    12/21 プロフィール未登録ユーザーの入室を禁止する処理

--- src/app/poker/room/room.page.ts
+++ src/app/poker/room/room.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { FirestoreService, IUser } from 'src/app/shared/firestore.service';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-room',
@@ -8,11 +11,40 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class RoomPage implements OnInit {
   roomId: string;
+  user: IUser;
+  uid: string;
 
-  constructor(public route: ActivatedRoute) { }
+  constructor(
+    public route: ActivatedRoute,
+    public alertController: AlertController,
+    public navController: NavController,
+    public auth: AuthService,
+    public firestore: FirestoreService,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => this.roomId = params.get('roomId'));
+    // authからuidを取得し、firestoreのデータを確認
+    const user = await this.firestore.userInit( this.auth.getUserId() );
+    if (!user) {
+      const alert = await this.alertController.create({
+        header: 'プロフィール登録が必要です。',
+        buttons: [
+          {
+            text: '閉じる',
+            role: 'cancel',
+            handler: data => this.navController.navigateBack('/poker')
+          }
+        ]
+      });
+      alert.present();
+    }
+  }
+
+  // ユーザ情報を取得
+  async ionViewWillEnter() {
+    this.uid = this.auth.getUserId();
+    this.user = await this.firestore.userInit(this.uid);
   }
 
 }
